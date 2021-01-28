3bc662a50dcdf2a54b6ceb00411864d0cd320e41
truth809 <truth809@naver.com>
2021-01-28 19:35:36 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
+
+class Gugudan extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+      value: '',
+      result: '',
+    };
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    if(parseInt(this.state.value) === this.state.first * this.state.second) {
+      this.setState({
+        result: this.state.value+' 정답',
+        first: Math.ceil(Math.random() * 9),
+        second: Math.ceil(Math.random() * 9),
+        value: '',
+      });
+    } else {
+      this.setState({
+        result: '땡',
+        value: '',
+      });
+    }
+  };
+  onChange = (e) => this.setState({ value: e.target.value});
+  render() {
+    return (
+      <div>
+        <div>{this.state.first} 곱하기 {this.state.second} 는?</div>
+        <form onSubmit={this.onSubmit}>
+          <input type="number" value={this.state.value} onChange={this.onChange}></input>
+          <button>입력!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gugudan />
     </div>
   );
 }
