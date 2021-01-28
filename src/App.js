ed451ee08155aef61f97342607ca03e3f982045d
truth809 <truth809@naver.com>
2021-01-28 20:26:05 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -16,26 +16,39 @@ class Gugudan extends React.Component {
   onSubmit = (e) => {
     e.preventDefault();
     if(parseInt(this.state.value) === this.state.first * this.state.second) {
-      this.setState({
-        result: this.state.value +' 정답',
-        first: Math.ceil(Math.random() * 9),
-        second: Math.ceil(Math.random() * 9),
-        value: '',
+      this.setState((prevState) => {
+        return {
+          result: '정답: ' + prevState.value, //this.state.value를 간편하게
+          first: Math.ceil(Math.random() * 9),
+          second: Math.ceil(Math.random() * 9),
+          value: '',
+        }
       });
+      this.input.focus();
     } else {
       this.setState({
         result: '땡',
         value: '',
       });
+      this.input.focus();
     }
   };
-  onChange = (e) => this.setState({ value: e.target.value});
+  onChange = (e) => {
+    this.setState({ value: e.target.value});
+  };
+  onRefInput = (c) => {this.input = c;};
+
+  input;
+  
+  // setState를 할때마다 렌더 실행
+  // 렌더를 많이하면 느려짐 주의
+  // 렌더 안에 함수 밖으로 빼주기, 안빼주면 함수가 새로 계속 생기게됨
   render() {
     return (
       <>
         <div>{this.state.first} 곱하기 {this.state.second} 는?</div>
         <form onSubmit={this.onSubmit}>
-          <input type="number" value={this.state.value} onChange={this.onChange}></input>
+          <input ref={this.onRefInput} type="number" value={this.state.value} onChange={this.onChange}></input>
           <button>입력!</button>
         </form>
         <div>{this.state.result}</div>
