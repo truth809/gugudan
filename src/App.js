341a0498f5a99c54a1140db36a9f909c1093cb0c
truth809 <truth809@naver.com>
2021-01-28 19:41:12 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class Gugudan extends React.Component {
     e.preventDefault();
     if(parseInt(this.state.value) === this.state.first * this.state.second) {
       this.setState({
-        result: this.state.value+' 정답',
+        result: this.state.value +' 정답',
         first: Math.ceil(Math.random() * 9),
         second: Math.ceil(Math.random() * 9),
         value: '',
@@ -32,14 +32,14 @@ class Gugudan extends React.Component {
   onChange = (e) => this.setState({ value: e.target.value});
   render() {
     return (
-      <div>
+      <>
         <div>{this.state.first} 곱하기 {this.state.second} 는?</div>
         <form onSubmit={this.onSubmit}>
           <input type="number" value={this.state.value} onChange={this.onChange}></input>
           <button>입력!</button>
         </form>
         <div>{this.state.result}</div>
-      </div>
+      </>
     );
   }
 }
